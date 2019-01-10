445f5e7c4b8ae0a84ea531c01730b947b79dc3ac
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-10 15:19:53 -0800

    Fixed replace_third_and_last, replace middle functions

--- list_operations.py
+++ list_operations.py
@@ -168,8 +168,9 @@ def replace_third_and_last(input_list):
     True
 
     """
-
-    pass
+    input_list[2] = 37
+    input_list[-1] = 37
+    return
 
 
 def replace_middle(input_list):
@@ -188,7 +189,10 @@ def replace_middle(input_list):
 
     """
 
-    pass
+    input_list[2:-3] = [42]
+    input_list[-3] = 37
+
+    return
 
 
 def delete_third_and_seventh(input_list):
